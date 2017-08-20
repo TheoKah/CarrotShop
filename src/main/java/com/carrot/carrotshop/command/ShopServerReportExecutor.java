a0c59dbd6bf3d838654c56628181d8b04a7e234b
Kah <t.sbrissa@gmail.com>
2017-08-20 19:17:26 +0200

    Add ShopServerReportExecutor.java

---
+++ src/main/java/com/carrot/carrotshop/command/ShopServerReportExecutor.java
@@ -0,0 +1,27 @@
+package com.carrot.carrotshop.command;
+
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
+
+import com.carrot.carrotshop.ShopsLogs;
+
+public class ShopServerReportExecutor implements CommandExecutor{
+
+	@Override
+	public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+		if (!src.hasPermission("carrotshop.report.server")) {
+			src.sendMessage(Text.of(TextColors.DARK_RED, "You do not have permission to generate reports for other the server"));
+			return CommandResult.success();
+		}		
+
+		ShopsLogs.generateReport(src, null);
+
+		return CommandResult.success();
+	}
+
+}
