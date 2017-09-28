53ea69290f445f477522a513d0dc4146e2fb02a0
Kah <t.sbrissa@gmail.com>
2017-09-28 23:14:12 +0200

    Protect blocks supporting active signs

--- src/main/java/com/carrot/carrotshop/listener/BlockBreakListener.java
+++ src/main/java/com/carrot/carrotshop/listener/BlockBreakListener.java
@@ -5,12 +5,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.spongepowered.api.block.BlockSnapshot;
+import org.spongepowered.api.block.BlockTypes;
 import org.spongepowered.api.data.Transaction;
+import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.Order;
 import org.spongepowered.api.event.block.ChangeBlockEvent;
 import org.spongepowered.api.event.block.tileentity.ChangeSignEvent;
+import org.spongepowered.api.util.Direction;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 
@@ -19,7 +22,26 @@ import com.carrot.carrotshop.shop.Shop;
 
 public class BlockBreakListener {
 
-	@Listener
+	private boolean isSignAndShop(Location<World> loc, Direction dir, boolean wall) {
+		if (loc.getRelative(dir).getBlockType() == (wall ? BlockTypes.WALL_SIGN : BlockTypes.STANDING_SIGN)) {
+			if (wall) {
+				Location<World> sign = loc.getRelative(dir);
+				if (sign.supports(Keys.DIRECTION)) {
+					Optional<Direction> direction = sign.get(Keys.DIRECTION);
+					if (direction.isPresent()) {
+						if (direction.get() != dir)
+							return false;
+					}
+				}
+			}
+			Optional<List<Shop>> shops = ShopsData.getShops(loc.getRelative(dir));
+			if (shops.isPresent())
+				return true;
+		}
+		return false;
+	}
+
+	@Listener(order = Order.FIRST, beforeModifications = true)
 	public void onBlockBreak(ChangeBlockEvent.Break event) {
 		for (Transaction<BlockSnapshot> transaction : event.getTransactions()) {
 			if (transaction.isValid()) {
@@ -39,6 +61,12 @@ public class BlockBreakListener {
 							if (!shop.destroy(cause.get()))
 								event.setCancelled(true);
 						});
+					} else if (isSignAndShop(loc.get(), Direction.UP, false) ||
+							isSignAndShop(loc.get(), Direction.NORTH, true) ||
+							isSignAndShop(loc.get(), Direction.SOUTH, true) ||
+							isSignAndShop(loc.get(), Direction.EAST, true) ||
+							isSignAndShop(loc.get(), Direction.WEST, true)) {
+						event.setCancelled(true);
 					}
 				}
 			}
