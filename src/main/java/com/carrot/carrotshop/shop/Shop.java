75ff5a1a29f02c538df249e143985e040d18dba5
Kah <t.sbrissa@gmail.com>
2018-12-23 22:01:11 +0100

    Add new [cmd] sign

--- src/main/java/com/carrot/carrotshop/shop/Shop.java
+++ src/main/java/com/carrot/carrotshop/shop/Shop.java
@@ -355,6 +355,9 @@ public abstract class Shop {
 					case "[heal]":
 						shop = new Heal(player, target);
 						break;
+					case "[cmd]":
+						shop = new Cmd(player, target);
+						break;
 					case "[deviceon]":
 						shop = new DeviceOn(player, target);
 						break;
