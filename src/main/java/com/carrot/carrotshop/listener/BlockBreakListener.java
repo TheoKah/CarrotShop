5ca2688bd5959ea5b0de1be4d314cc6f28b19143
Kah <t.sbrissa@gmail.com>
2020-09-23 22:22:55 +0200

    Fix dupe glitch with shop protection

--- src/main/java/com/carrot/carrotshop/listener/BlockBreakListener.java
+++ src/main/java/com/carrot/carrotshop/listener/BlockBreakListener.java
@@ -4,9 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.spongepowered.api.block.BlockSnapshot;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.block.BlockTypes;
-import org.spongepowered.api.data.Transaction;
 import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
@@ -21,8 +20,10 @@ import com.carrot.carrotshop.ShopsData;
 import com.carrot.carrotshop.shop.Shop;
 
 public class BlockBreakListener {
+	boolean preCanceled = false;
+	int preCanceledTick = -1;
 
-	private boolean isSignAndShop(Location<World> loc, Direction dir, boolean wall) {
+	private void addSignAndShop(List<Shop> allShops, Location<World> loc, Direction dir, boolean wall) {
 		if (loc.getRelative(dir).getBlockType() == (wall ? BlockTypes.WALL_SIGN : BlockTypes.STANDING_SIGN)) {
 			if (wall) {
 				Location<World> sign = loc.getRelative(dir);
@@ -30,48 +31,52 @@ public class BlockBreakListener {
 					Optional<Direction> direction = sign.get(Keys.DIRECTION);
 					if (direction.isPresent()) {
 						if (direction.get() != dir)
-							return false;
+							return;
 					}
 				}
 			}
 			Optional<List<Shop>> shops = ShopsData.getShops(loc.getRelative(dir));
 			if (shops.isPresent())
-				return true;
+				allShops.addAll(shops.get());
 		}
-		return false;
+	}
+
+	@Listener(order = Order.FIRST, beforeModifications = true)
+	public void onBlockBreak(ChangeBlockEvent.Pre event) {
+		List<Shop> allShops = new ArrayList<>();
+		preCanceledTick = Sponge.getServer().getRunningTimeTicks();
+		preCanceled = false;
+		for (Location<World> loc : event.getLocations()) {
+			Optional<List<Shop>> shops = ShopsData.getShops(loc);
+			if (shops.isPresent()) {
+				allShops.addAll(shops.get());
+			}
+			addSignAndShop(allShops, loc, Direction.UP, false);
+			addSignAndShop(allShops, loc, Direction.NORTH, true);
+			addSignAndShop(allShops, loc, Direction.SOUTH, true);
+			addSignAndShop(allShops, loc, Direction.EAST, true);
+			addSignAndShop(allShops, loc, Direction.WEST, true);
+		}
+		if (allShops.isEmpty())
+			return;
+		Optional<Player> cause = event.getCause().first(Player.class);
+		if (cause.isPresent()) {
+			allShops.forEach((shop) -> {
+				if (!shop.destroy(cause.get())) {
+					event.setCancelled(true);
+					preCanceled = true;
+				}
+			});
+		} else {
+			event.setCancelled(true);
+			preCanceled = true;
+		}
 	}
 
 	@Listener(order = Order.FIRST, beforeModifications = true)
 	public void onBlockBreak(ChangeBlockEvent.Break event) {
-		for (Transaction<BlockSnapshot> transaction : event.getTransactions()) {
-			if (transaction.isValid()) {
-				Optional<Location<World>> loc = transaction.getOriginal().getLocation();
-				if (loc.isPresent()) {
-					Optional<List<Shop>> shops = ShopsData.getShops(loc.get());
-					if (shops.isPresent()) {
-						Optional<Player> cause = event.getCause().first(Player.class);
-						if (cause.isPresent()) {
-							List<Shop> toDelete = new ArrayList<>();
-							shops.get().forEach((shop) -> {
-								toDelete.add(shop);
-							});
-							toDelete.forEach((shop) -> {
-								if (!shop.destroy(cause.get()))
-									event.setCancelled(true);
-							});
-						} else {
-							event.setCancelled(true);
-						}
-					} else if (isSignAndShop(loc.get(), Direction.UP, false) ||
-							isSignAndShop(loc.get(), Direction.NORTH, true) ||
-							isSignAndShop(loc.get(), Direction.SOUTH, true) ||
-							isSignAndShop(loc.get(), Direction.EAST, true) ||
-							isSignAndShop(loc.get(), Direction.WEST, true)) {
-						event.setCancelled(true);
-					}
-				}
-			}
-		}
+        if (preCanceled && preCanceledTick == Sponge.getServer().getRunningTimeTicks())
+            event.setCancelled(true);
 	}
 
 	@Listener(order = Order.FIRST, beforeModifications = true)
